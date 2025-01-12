7047ff2979db854204ed8b0ee506d313bd03ea15
Dhruv Kotadiya <22ce054@charusat.edu.in>
2025-01-12 09:16:37 +0530

    Removed Unused Dependency and Moved to Dep Dependencies

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,7 +1,6 @@
-import { Request, Response, NextFunction } from "express";
-import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
-import { getCurrentUser } from "lib/user";
+import { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
 dotenv.config();
 const JWT_SECRET = process.env.JWT_SECRET;
 
